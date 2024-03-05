2430d0484dae9449a460e82ee09ba616d0f1ff97
Yuri Prokopenko <yuriprokopenko84@gmail.com>
2024-03-05 16:56:57 +0200

    added transition group and added animation for deleting and adding posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,3 +1,4 @@
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import PostItem from "./PostItem";
 
 import styles from "./PostList.module.css";
@@ -5,15 +6,18 @@ import styles from "./PostList.module.css";
 const PostList = ({ posts, onDeletePost }) => {
   return (
     <ul className={styles["wrapper"]}>
-      {posts.map((p, index) => (
-        <PostItem
-          key={p.id}
-          id={p.id}
-          name={`${index + 1}. ${p.name}`}
-          description={p.description}
-          onDeletePost={onDeletePost}
-        />
-      ))}
+      <TransitionGroup component={null}>
+        {posts.map((p, index) => (
+          <CSSTransition key={p.id} timeout={400} classNames="post">
+            <PostItem
+              id={p.id}
+              name={`${index + 1}. ${p.name}`}
+              description={p.description}
+              onDeletePost={onDeletePost}
+            />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </ul>
   );
 };
