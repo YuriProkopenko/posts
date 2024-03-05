6380139a76cef0ad172724bc51e72016accee48f
Yuri Prokopenko <yuriprokopenko84@gmail.com>
2024-03-05 18:18:05 +0200

    created an animated posts title

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,20 +5,27 @@ import styles from "./PostList.module.css";
 
 const PostList = ({ posts, onDeletePost }) => {
   return (
-    <ul className={styles["wrapper"]}>
-      <TransitionGroup component={null}>
-        {posts.map((p, index) => (
-          <CSSTransition key={p.id} timeout={400} classNames="post">
-            <PostItem
-              id={p.id}
-              name={`${index + 1}. ${p.name}`}
-              description={p.description}
-              onDeletePost={onDeletePost}
-            />
-          </CSSTransition>
-        ))}
-      </TransitionGroup>
-    </ul>
+    <div className={styles["wrapper"]}>
+      <ul className={styles["list"]}>
+        <TransitionGroup component={null}>
+          {posts.map((p, index) => (
+            <CSSTransition key={p.id} timeout={400} classNames="post">
+              <PostItem
+                id={p.id}
+                name={`${index + 1}. ${p.name}`}
+                description={p.description}
+                onDeletePost={onDeletePost}
+              />
+            </CSSTransition>
+          ))}
+          {!posts.length && (
+            <CSSTransition timeout={400} classNames="postsTitle">
+              <p className={styles["title"]}>No more posts left</p>
+            </CSSTransition>
+          )}
+        </TransitionGroup>
+      </ul>
+    </div>
   );
 };
 
